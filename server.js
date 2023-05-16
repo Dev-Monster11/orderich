dd6fccf0ac2b60ef0636ffd12d0df3177f77640e
Dev-Monster11 <softdrink1991@gmail.com>
2023-05-16 12:56:00 -0500

    test

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const path = require('path');
 
 const zipFile = archiver('zip', { zlib: { level: 9 }});
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "build")));

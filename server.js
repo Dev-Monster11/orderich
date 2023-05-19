2da1986e1a7427d8a8368dfc551f3e248e30f8a5
Dev-Monster11 <softdrink1991@gmail.com>
2023-05-19 14:48:26 -0500

    test

--- server.js
+++ server.js
@@ -2,23 +2,14 @@
 
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer');
 const app = express();
 const shell = require('shelljs');
 const fs = require('fs');
 const archiver = require('archiver');
 const path = require('path');
-
+const formidable = require('formidable');
 const zipFile = archiver('zip', { zlib: { level: 9 }});
 
-
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,34 +18,20 @@ app.use(cors({
     origin: '*'
 }));
 
-// var storage = multer.diskStorage({
-//     destination: (_, file, cb) => {
-//         cb(null, 'uploads');
-//         // console.log('upload: saved to ' + file.originalname);
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, "temp.png");
-//     }
-// })
-// var upload = multer({storage: storage});
-let upload = multer({
-    dest: 'uploads/'
-});
 app.use(express.static(path.join(__dirname, "build")));
 app.get('/appmanager', (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 })
-app.post('/appmanager/api/upload', upload.single("file"), (req, res) => {
-    
-    fs.readFile(req.file.path, (err, data) => {
-        fs.writeFile("temp.png", data, err => {
-            if (err) res.json(err);
-        });
+app.post('/appmanager/api/upload', (req, res) => {
+    const form = new formidable.IncomingForm();
+    form.uploadDir = path.join(__dirname, 'uploads');
+    form.parse(req);
+    form.on('file', (err, file) => {
+        res.send(file.newFilename);
     });
-    res.json('success');
 });
 app.post('/appmanager/api/generate', (req, res) => {
-    // shell.exec('npm i -g react-native-cli');
+
     const skeleton = 'react-native init --npm --skip-install ' + req.body.name;
     shell.config.silent=true;
     shell.exec(skeleton, function(code, stdout, stderr) {
@@ -62,17 +39,17 @@ app.post('/appmanager/api/generate', (req, res) => {
         var data = fs.readFileSync(path.join(__dirname, req.body.name, 'App.tsx'), 'utf8');
         var result = data.replace('uri: ', 'uri: "' + req.body.url + '"');
         fs.writeFileSync(path.join(__dirname, req.body.name, 'App.tsx'), result, 'utf8');
-        // console.log('App.tsx copy finished');
+        console.log('App.tsx copy finished');
         shell.cd(req.body.name);
         shell.mv('package.json', 'backup.json');
         shell.exec('npm init -f');
         shell.exec('npm i -D @bam.tech/react-native-make');
-        // console.log(' install done');
-        command = 'react-native set-icon --platform android --path ../temp.png';
+        console.log(' install done');
+        command = 'react-native set-icon --platform android --path ../uploads/' + req.body.logo;
         shell.exec(command);
-        command = 'react-native set-icon --platform ios --path ../temp.png';
+        command = 'react-native set-icon --platform ios --path ../uploads' + req.body.logo;
         shell.exec(command);
-        // console.log('Icon Changed');
+        console.log('Icon Changed');
         shell.mv('backup.json', 'package.json');
         shell.exec('add-dependencies react-native-webview');
         shell.exec('add-dependencies @bam.tech/react-native-make -D')
@@ -86,13 +63,13 @@ app.post('/appmanager/api/generate', (req, res) => {
         zipFile.directory(path.join(__dirname, req.body.name), false);
         zipFile.finalize()
         .then(() => {
-            // console.log('zip file create');
+            console.log('zip file create');
 
             fs.rmSync(path.join(__dirname, req.body.name), {recursive: true});
             fs.rmSync(`${__dirname}/temp.png`);
             // res.download(path.join(__dirname, `${req.body.name}.zip`));
-            res.send('success');
-            // res.sendFile(`${req.body.name}.zip`, { root: `${__dirname}`});
+            // res.send('success');
+            res.sendFile(path.join(__dirname, req.body.name + ".zip"));
         });
     });
   
